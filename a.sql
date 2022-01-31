65c6b89b4efe32f6b82b8b3a6d128539463878f6
jecastrom <jcastro.de@outlook.com>
2022-01-31 20:42:47 +0100

    s

---
+++ a.sql
@@ -0,0 +1,22 @@
+SELECT
+    loan_id,
+    account_id,
+    format(amount, 2, 'de_DE') AS loan_amount
+FROM
+    (
+        SELECT
+            *,
+            row_number() over (
+                ORDER BY
+                    amount DESC
+            ) AS `row`
+        FROM
+            loan
+    ) t
+WHERE
+    `row` <= (
+        SELECT
+            count(*) * (0.1)
+        FROM
+            loan
+    );
