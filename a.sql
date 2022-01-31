eccc9607d9418a52d1359946a929191065f3ae51
jecastrom <jcastro.de@outlook.com>
2022-01-31 21:38:54 +0100

    s

--- a.sql
+++ a.sql
@@ -1,24 +1,12 @@
 SELECT
     loan_id,
     account_id,
-    format(amount, 2, 'de_DE') AS loan_amount
+    format(amount, 2, 'de_DE') AS loan_amount,
+    row_number() over (
+        ORDER BY
+            amount DESC
+    ) AS `row`
 FROM
-    (
-        SELECT
-            *,
-            row_number() over (
-                ORDER BY
-                    amount DESC
-            ) AS `row`
-        FROM
-            loan
-    ) t
-WHERE
-    `row` <= (
-        SELECT
-            count(*) * (0.1)
-        FROM
-            loan
-    )
+    loan
 LIMIT
-    5;
+    10;
